caa2da6b9895a8e7a34f502e13c6f15ec886a791
ThinkBig <1243756230@qq.com>
2019-09-28 21:03:36 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,36 @@
+const VueSSRServerPlugin = require('vue-server-renderer/server-plugin')
+const VueSSRClientPlugin = require('vue-server-renderer/client-plugin')
+const nodeExternals = require('webpack-node-externals')
+const merge = require('lodash.merge')
+const TARGET_NODE = process.env.WEBPACK_TARGET === "node"
+const target = TARGET_NODE ? 'server' : 'client'
+
+module.exports = {
+  css: {
+    extract: false
+  },
+  outputDir: './dist/' + target,
+  configurewebpack: () => ({
+    entry: `./src/entry/entry-${target}.js`,
+    devtool: 'source-map',
+    target: TARGET_NODE ? "node" : "web",
+    node: target ? undefined : false,
+    output: {
+      libraryTarget: TARGET_NODE ? "commonjs2" : undefined
+    },
+    externals: TARGET_NODE ? nodeExternals({
+      whitelist: [/\.css$/]
+    }) : undefined,
+    optimization: {
+      splitChunks: undefined
+    },
+    plugins: [TARGET_NODE ? new VueSSRServerPlugin() : new VueSSRClientPlugin()]
+  }),
+  chainwebpack: config => {
+    config.module.rule('vue').use('vue-loader').tap(options => {
+      merge(options, {
+        optimizeSSR: false
+      })
+    })
+  }
+}
